2c9c160e529f4c2d334c9cba35c4fafff3403ea0
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-29 23:50:59 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     upper = ""
-    for i in str:
+    for i in range(len(str)):
         if (97 <= ord(str[i]) and 122 >= ord(str[i])):
             upper += chr(ord(str[i]) - 32)
         else:
             upper += str[i]
-    print("{0}".format(upper))
+    print("{}".format(upper))
