1a9c75cf832f7f26dadcd086152374047ba09eee
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-01 17:08:46 -0800

    0-add.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-if __name__ = "__main__":
+if __name__ == '__main__':
     a = 10
     b = 5
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
