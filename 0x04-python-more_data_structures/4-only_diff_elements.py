477b32cdcee192d2473f2f010d93f84b54d5b400
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-07 19:12:39 -0800

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    diff_set = set()
-    diff_set set_1.difference(set_2)
-    return (diff_set)
+    diff = set()
+    diff = set_1.difference(set_2)
+    diff = set((list(diff)).sort())
+    return (diff)
