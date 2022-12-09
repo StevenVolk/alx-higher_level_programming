c2b2717a3bc712857efff70ce3da99551b331f42
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-09 06:17:52 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,10 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return (None)
-    best = a_dictionary[0][0]
+    best = None
     for key, value in a_dictionary:
+        if best == None:
+            best = key
         if a_dictionary[key] > a_dictionary[best]:
             best = key
     return (best)
