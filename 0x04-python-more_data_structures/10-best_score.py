8954e2f72c11fb683a2d8001640191f07bddb151
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-09 06:23:27 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return (None)
-    best = max(a_dictionary.values(), default=None)
+    best = max(a_dictionary.values())
     for key, value in a_dictionary:
         if best == value:
             return (key)
