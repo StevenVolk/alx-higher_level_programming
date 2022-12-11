5388d819cf8e83718f24cd351e3870e9662148ca
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-11 03:41:43 -0800

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list:
+        scores = 0
+        weight = 0
+        for score, weight in my_list:
+            scores += score * weight
+            weights += weight
+        w_average = scores / weights
+        return (w_average)
+    else:
+        return (0)
