340504d0f6b22c5693217903cea59b706c1f32b9
Adedimeji <ojerindeadedimeji@gmail.com>
2022-12-11 03:54:31 -0800

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda inner_list : list(map(lambda i : i * i, inner_list)), matrix))
+    return list(map(lambda inner: list(map(lambda i: i * i, inner)), matrix))
