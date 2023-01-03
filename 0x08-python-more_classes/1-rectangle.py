8bb93fd1093fd9f92c779afeefe29a93f5b3ffc4
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-03 07:09:57 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+
+A rectangle class
+
+"""
+
+
+class Rectangle:
+    def __init__(self, width = 0, height = 0):
+        width(width)
+        height(height)
+
+    def width(self):
+        """
+
+        returns the value of width
+
+        """
+
+        return self.width
+
+    def width(self, value):
+        """
+
+        assigns a value to width
+
+        """
+
+        if self.check_value_int(value) is False:
+            raise TypeError("width must be an integer")
+
+        if self.check_positive(value) is False:
+            raise ValueError("width must be >= 0")
+
+        self.width = width
+
+    def height(self):
+        """
+
+        returns the value of height
+
+        """
+
+        return self.height
+
+    def height(self, height):
+        """
+
+        assigns a value to height
+
+        """
+
+        if self.check_value_int(value) is False:
+            raise TypeError("height must be an integer")
+
+        if self.check_positive(value) is False:
+            raise ValueError("height must be >= 0")
+
+        self.height = height
+
+    def check_value_int(value):
+        """
+
+        checks if a value is of type int
+
+        """
+        
+        if type(value) is int:
+            return True
+        return False
+
+    def check_positive(value):
+        """
+
+        check if a value is greater or equal to zero
+
+        """
+
+        if value >= 0:
+            return True
+        return False
