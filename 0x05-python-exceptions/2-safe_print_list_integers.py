6008fee44744c846e1619cebddf18b6f61eeef88
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-23 00:59:09 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
-            j++
+            print("{:d}".format(my_list[i]), end="")
         except Exception:
             continue
     return(j)
