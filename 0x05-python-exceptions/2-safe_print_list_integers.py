c7598f7f1525e541b6d231cb007c45e80e4f1a44
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-23 01:14:39 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             j = j + 1
         except ValueError:
             continue
+        except TypeError:
+            continue
     print()
     return(j)
