d8101fb811d00167683978f4d585e5ee02215bbc
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-23 01:55:02 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         print("Inside result: None")
-        return("None")
+        result = "None"
     finally:
         print("Inside result: {}".format(result))
-        return(result)
+    return(result)
