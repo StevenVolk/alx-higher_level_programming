da5ee2dc646d354986403ef96546f90d632d54df
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-23 23:45:14 -0800

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return(fct(*args))
     except Exception as error:
-        sys.stderr.write("Exception {}".format(error))
+        sys.stderr.write("Exception {}\n".format(error))
         return(None)
