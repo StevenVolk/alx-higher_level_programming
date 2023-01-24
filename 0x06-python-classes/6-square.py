a658b783505ab7f666d2ae1efbfbdc1a8ef16f05
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-24 06:02:15 -0800

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+
+A class Square
+
+"""
+
+
+class Square:
+    """
+
+    A class Square
+
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+
+        if len(postion) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.position = position
+
+    def area(self):
+        return(self.size ** 2)
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        return(self.__position)
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.position = value
