22ae122e998062440d48e2ac2946d9a3d460a457
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-24 00:29:37 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,10 +14,12 @@ class Square:
     """
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     def area(self):
-        return(self.__size ** 2)
+        if self.__size:
+            return(self.__size ** 2)
+        return self.size * self.size
 
     def size(self):
         return(self.__size)
