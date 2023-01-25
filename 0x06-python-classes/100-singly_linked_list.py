ba921e3134ab901640949df921a38c44800298e1
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-25 06:25:54 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -33,6 +33,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
+        print(type(Node))
         if type(value) is not Node or value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
