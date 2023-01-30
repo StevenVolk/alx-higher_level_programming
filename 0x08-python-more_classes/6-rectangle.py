5854aaa0d948b23fe10dbcb9925f982812ee82bd
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-29 22:51:34 -0800

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         global number_of_instances
-        number_of_instances += 1
+        type(self).number_of_instances += 1
         self.height = height
         self.width = width
 
