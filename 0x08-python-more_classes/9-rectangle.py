ebcbbabd318292634b620137ec0c0b9a0ad5e82e
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-30 01:19:55 -0800

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -142,5 +142,4 @@ class Rectangle:
         return rect_2
 
     def square(cls, size=0):
-        (cls).size = size
-        return self.Rectangle((cls).size, (cls).size)
+        return self.Rectangle(cls, size, size)
