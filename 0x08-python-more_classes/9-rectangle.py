a041fb87438fa33490add7f371834454164df2c7
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-30 18:10:57 -0800

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -19,8 +19,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         global number_of_instances
         type(self).number_of_instances += 1
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
