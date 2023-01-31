221af6c1ce9a3a8c6ad98d66e0007b867d787a16
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-30 18:28:55 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,10 +14,10 @@ def add_integer(a, b=98):
 
     """
     
-    if type(a) is not float or type(a) is not int:
+    if type(a) is not float and type(a) is not int:
         raise TypeError("a must be an integer")
 
-    if type(b) is not float or type(b) is not int:
+    if type(b) is not float and type(b) is not int:
         raise TypeError("b must be an integer")
 
     if type(a) is float and type(b) is float:
