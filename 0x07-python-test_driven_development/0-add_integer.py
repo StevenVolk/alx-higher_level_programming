7057292483983445067ac0454d28e28d92ab3d20
Adedimeji <ojerindeadedimeji@gmail.com>
2023-01-30 18:23:43 -0800

    new_files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+
+a function that adds 2 integers.
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+
+    a method that adds 2 integers
+
+    """
+    
+    if type(a) is not float or type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not float or type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    if type(a) is float and type(b) is float:
+        return(int(a) + int(b))
+    elif type(a) is float:
+        return(int(a) + b)
+    elif type(b) is float:
+        return(a + int(b))
+    else:
+        return(a + b)
