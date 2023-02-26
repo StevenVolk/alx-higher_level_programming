bc99daa3576909ef872b2fbbd1c83c75ca402b1a
Adedimeji <ojerindeadedimeji@gmail.com>
2023-02-26 02:44:19 -0800

    correction for validation

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,9 +26,9 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for num in row:
             if num is None or (type(num) is not int and
-                                type(num) is not float):
+                            type(num) is not float):
                 raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
+(list of lists) of integers/floats")
             new_num = round(num / div, 2)
             new_row.append(new_num)
         new_matrix.append(new_row)
