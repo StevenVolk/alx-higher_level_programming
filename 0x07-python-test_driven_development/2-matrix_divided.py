249af12e0e0d0ff1238f80bf13ed8b9baad8f724
Adedimeji <ojerindeadedimeji@gmail.com>
2023-02-26 02:26:48 -0800

    new_files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+
+A function that divides all elements of a matrix
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+
+    A method that divides all elements of a matrix.
+
+    """
+
+
+    if div is None or (type(div) is not float and Type(div) is not int):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    n_row = len(matrix[0])
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        if n_row != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for num in row:
+            if num is None or (Type(num) is not int and Type(num) is not float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_num = round(num / div, 2)
+            new_row.append(new_num)
+        new_matrix.append(new_row)
+
+    return(new_matrix)
