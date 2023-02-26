9a0f9a0240b5765649a716e07e695f1f5bed81fb
Adedimeji <ojerindeadedimeji@gmail.com>
2023-02-26 04:14:15 -0800

    corrections

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,12 @@ A function that prints My name is <first name> <last name>
 
 
 def say_my_name(first_name, last_name=""):
+    """
+
+    A method that prints My name is <first name> <last name>
+
+    """
+
     if type(first_name) is None or type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
