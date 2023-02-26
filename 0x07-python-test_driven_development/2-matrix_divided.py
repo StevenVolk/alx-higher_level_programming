b10a40cccb2baa0e15e7b148913072768b2a5f0f
Adedimeji <ojerindeadedimeji@gmail.com>
2023-02-26 02:30:33 -0800

    changed uppercase letter T in type to lowercase

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,7 +15,7 @@ def matrix_divided(matrix, div):
     """
 
 
-    if div is None or (type(div) is not float and Type(div) is not int):
+    if div is None or (type(div) is not float and type(div) is not int):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -26,7 +26,7 @@ def matrix_divided(matrix, div):
         if n_row != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         for num in row:
-            if num is None or (Type(num) is not int and Type(num) is not float):
+            if num is None or (type(num) is not int and type(num) is not float):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             new_num = round(num / div, 2)
             new_row.append(new_num)
