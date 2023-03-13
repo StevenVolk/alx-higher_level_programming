36016a45946ba1538cf99ee08dec86d20ea8f320
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-13 03:31:31 -0700

    new

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+
+A class MyList that inherits from list
+
+"""
+
+
+class MyList(list):
+    """
+
+    A class MyList that inherits from list
+
+    """
+
+    def print_sorted(self):
+        """
+
+        This method prints the list, but sorted (ascending sort)
+
+        """
+
+        self.sort()
