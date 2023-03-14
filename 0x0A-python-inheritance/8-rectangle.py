dd7745094cc4a8f077ac32f7c113254f2747db05
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-13 19:54:43 -0700

    new

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+
+A class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+
+    A class Rectangle that inherits from BaseGeometry
+
+    """
+
+    def __init__(self, width, height):
+        if BaseGeometry.integer_validator(self, width):
+            self.width = width
+        if BeseGeometry.integer_validator(self, height):
+            self.height = height
