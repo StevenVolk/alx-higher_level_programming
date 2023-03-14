796016a5c949b4c348ef790c426e13a5e9ab305d
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-13 18:47:16 -0700

    correction

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
 
     """
 
-    def area():
+    def area(self):
         """
 
         A method that raises an Exception with the message area() is not
@@ -31,7 +31,7 @@ class BaseGeometry:
         """
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("<name> must be an integer")
 
         if value >= 0:
             raise ValueError("{} must be greater than 0".format(name))
