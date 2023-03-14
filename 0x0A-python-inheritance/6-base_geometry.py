49eb2da6fac0bffafe1e2e2c340465c4cb4c05a2
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-13 18:22:29 -0700

    new

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+
+A class BaseGeometry (based on 5-base_geometry.py)
+
+"""
+
+
+class BaseGeometry:
+    """
+
+    A class BaseGeometry
+
+    """
+
+    def area(self):
+        """
+
+        A method that raises an Exception with the message area() is not
+        implemented
+
+        """
+
+        raise Exception("area() is not implemented")
