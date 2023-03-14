db265c1efb35ffc306682bb6a06b80ad046990a6
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-13 18:38:18 -0700

    new

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+
+A class BaseGeometry (based on 6-base_geometry.py)
+
+"""
+
+class BaseGeometry:
+    """
+
+    A class BaseGeometry
+
+    """
+
+    def area():
+        """
+
+        A method that raises an Exception with the message area() is not
+        implemented
+
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+
+        A method that validates value
+
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value >= 0:
+            raise ValueError("{} must be greater than 0".format(name))
