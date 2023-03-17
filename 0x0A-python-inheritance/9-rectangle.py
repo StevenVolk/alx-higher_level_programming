f610e3c61e12dd36b4fecd23599970423475a355
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-17 09:45:48 -0700

    new

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+
+A class BaseGeometry (based on 7-base_geometry.py).
+task based on 8-rectangle.py
+
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+
+A class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+
+    A class Rectangle that inherits from BaseGeometry
+
+    """
+
+    def __init__(self, width, height):
+        """
+
+        Inializing class
+
+        """
+
+        if BaseGeometry.integer_validator(self, "width", width):
+            self.width = width
+        if BaseGeometry.integer_validator(self, "height", height):
+            self.height = height
+
+    def area():
+        """
+
+        Calculates the area of rectangle (height * width)
+
+        """
+
+        return (self.height * self.width)
+
+    def __str__():
+        """
+
+        returns rectangle description: [Rectangle] <width>/<height>
+
+        """
+
+        return "[Rectangle] {}/{}".format(width, height)
