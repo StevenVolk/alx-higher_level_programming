d5f07442f7321564220596dc183b28fe61656724
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-18 12:39:45 -0700

    new

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+
+a function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+
+"""
+
+
+def append_write(filename="", text=""):
+    """
+
+    a function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+
+    """
+
+    with open(filename, a) as f:
+        g = f.write(text)
+        return g
