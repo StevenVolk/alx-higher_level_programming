45454e2b0781232b8dba61c1d15f25c509aad863
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-18 08:26:25 -0700

    changes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,4 @@
 -- second_table of the database hbtn_0c_0 in your MySQL server
 SELECT score, COUNT(score) AS 'number' FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC
+ORDER BY COUNT(score) DESC
