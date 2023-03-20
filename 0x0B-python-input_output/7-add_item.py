88ec24853ac782233d055ba44ed15aab5d0cf252
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-20 11:33:30 -0700

    new

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+
+Using function save_to_json_file from 5-save_to_json_file.py
+
+"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+"""
+
+Using function load_from_json_file from 6-load_from_json_file.py
+
+"""
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+"""
+
+importing sys
+
+"""
+
+import sys
+
+"""
+
+a script that adds all arguments to a Python list,
+and then save them to a file
+
+"""
+
+
+p_list = []
+
+for i in (1, len(sys.argv)):
+    p_list.append(sys.argv[i])
+
+my_list = load_from_json_file('add_item.json')
+mylist.extend(p_list)
+save_to_json_file(my_list, 'add_item.json')
