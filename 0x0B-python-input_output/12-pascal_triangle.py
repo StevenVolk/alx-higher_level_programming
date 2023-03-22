fffd13ad257a07aa53ec58d989107be3c0f877b5
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-22 11:24:59 -0700

    new

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+A function def pascal_triangle(n): that returns a list of lists of integers
+representing the Pascal’s triangle of n
+
+"""
+
+
+def pascal_triangle(n):
+    l_of_list = []
+
+    if n <= 0:
+        return l_of_list
+
+    for i in range(1, n+1):
+        l = []
+        for j in i:
+            if j == 0:
+                l.append(1)
+            elif j == i - 1:
+                l.append(1)
+            else:
+                l.append(prev[j] + prev[j-1])
+        l_of_list.append(l)
+        prev = l
+
+    return l_of_list
