d7c07ede007c6be43c7a0e66cb83b3823491df81
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-22 09:49:07 -0700

    new

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+
+A class Student that defines a student
+
+"""
+
+
+class Student:
+    """
+
+    A class Student that defines a student
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        attr_dict = self.__dict__
+        attr = dict()
+
+        if attrs is None:
+            return attr_dict
+
+        for att in attrs:
+            if att in attr_dict:
+                attr[att] = attr_dict[att]
+
+        return attr
+
+    def reload_from_json(self, json):
+        return self
