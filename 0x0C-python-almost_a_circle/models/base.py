7b36e0c19b98936f891cf6a085ce6dbe10383278
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 15:24:38 -0700

    new

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+models/base.py
+
+"""
+
+
+class Base:
+    """
+
+    class
+
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects++
+            self.id = __nb_objects
