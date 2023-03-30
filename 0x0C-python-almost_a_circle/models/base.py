3ec50ed8794388223202c5a2968c154b341c6d21
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 15:28:27 -0700

    new

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,12 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+
+        init
+
+        """
+
         if id != None:
             self.id = id
         else:
