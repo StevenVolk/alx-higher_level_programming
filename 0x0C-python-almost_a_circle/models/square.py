e3fe8d3869453b7f99dab552c75190311f9d7a78
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-31 07:24:22 -0700

    changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,33 @@ class Square(Rectangle):
         """ return [Square] (<id>) <x>/<y> - <size> """
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.height)
+
+    @property
+    def width(self):
+        """ getting width """
+        return self.width
+
+    @width.setter
+    def width(self, width):
+        """ setting width """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        """ getting height """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ setting height """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
