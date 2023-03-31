3a7ecd23739b30a30d157ca1356a1b91a7a4613d
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 18:14:02 -0700

    new

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+
+from models.base import Base
+
+"""
+
+
+from models.base import Base
+
+"""
+
+class Rectangle that inherits from Base
+
+"""
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_width(self):
+        return self.__width
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_height(self):
+        return self.__height
+
+    def set_x(self, x):
+        self.__x = x
+
+    def get_x(self):
+        return self.__x
+
+    def set_y(self, y):
+        self.__y = y
+
+    def get_y(self):
+        return self.__y
