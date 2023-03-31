9d46a2e9202dce4026d0fab7e4e2b5930a9c0df6
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-31 14:53:09 -0700

    dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -65,3 +65,7 @@ class Square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a Square """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
