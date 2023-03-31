fa79116818bc24f5291a0300d364d32910689d95
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 17:23:15 -0700

    changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,5 +19,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = __nb_objects
+            Base.nb_objects++
+            self.id = Base.nb_objects
