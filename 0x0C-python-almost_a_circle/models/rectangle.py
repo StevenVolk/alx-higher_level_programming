6f208c34c2162c5cd69a452f6b73ba10a2780678
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 20:09:48 -0700

    updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -159,3 +159,15 @@ class Rectangle(Base):
 
         return "[Rectangle] ({0}) {1}/{2} - {3}/{4}"\
             .format(self.id, self.__x, self.__y, self.__width, self.height)
+
+    def update(self, *args):
+        if len(*args) >= 5:
+            self.y(args[4])
+        if len(*args) >= 4:
+            self.x(args[3])
+        if len(*args) >= 3:
+            self.height(args[2])
+        if len(*args) >= 2:
+            self.width(args[1])
+        if len(*args) >= 1:
+            self.id(args[0])
