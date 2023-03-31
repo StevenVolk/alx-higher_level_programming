ede8b64441b024784167cedf4d5d8e815a6a26fb
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 17:17:29 -0700

    changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             __nb_objects = __nb_objects + 1
