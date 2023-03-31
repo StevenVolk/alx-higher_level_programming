b355d5af47fe44b8cccc6ab44c2aff2de97598da
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 18:18:19 -0700

    comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,26 +29,34 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    @setter
     def set_width(self, width):
         self.__width = width
 
+    @getter
     def get_width(self):
         return self.__width
 
+    @setter
     def set_height(self, height):
         self.__height = height
 
+    @getter
     def get_height(self):
         return self.__height
 
+    @setter
     def set_x(self, x):
         self.__x = x
 
+    @getter
     def get_x(self):
         return self.__x
 
+    @setter
     def set_y(self, y):
         self.__y = y
 
+    @getter
     def get_y(self):
         return self.__y
