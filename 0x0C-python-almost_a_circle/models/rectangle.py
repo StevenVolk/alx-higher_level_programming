d8d8424f5f83b236c9cd91f730d28ea55ad8aefa
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-30 18:16:01 -0700

    added comment

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,12 @@ class Rectangle that inherits from Base
 
 
 class Rectangle(Base):
+    """
+
+    class Rectangle that inherits from Base
+
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
