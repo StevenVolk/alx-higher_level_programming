e559ddefb1d1d80caf32f7f348baca1e82b92bb6
Adedimeji <ojerindeadedimeji@gmail.com>
2023-03-31 18:28:24 -0700

    changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,6 +62,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        """ returns a list of instances """
         filename = cls.__name__ + ".json"
         os.path.exists(filename) is False:
             return []
