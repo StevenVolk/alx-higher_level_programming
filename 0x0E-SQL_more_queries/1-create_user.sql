da35e1bc19d1e88a7bb95b0f0ec9b77e4bb4d58e
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-07 13:30:43 -0700

    changes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- a script that creates the MySQL server user user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
