7597c86806346ce8ac4fda4f7f3bb08ded75ff4b
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-08 09:13:27 -0700

    new

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,4 @@
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows, tv_genres, tv_show_genres
+WHERE tv_genres.name = 'Comedy' AND tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id;
