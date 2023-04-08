5dc23439c0c8c82d277f17103d54e94ae27a38f1
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-08 06:43:19 -0700

    changes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0d_usa and the table cities
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.cities(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE `hbtn_0d_usa`.cities(
+	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
 	name VARCHAR(256) NOT NULL)
