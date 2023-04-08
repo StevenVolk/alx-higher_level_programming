cf75bb29437795ff2f05bf1264fbaeb58beeb25e
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-08 07:38:19 -0700

    changes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have
 -- at least one genre linked
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 ORDER BY tv_shows.title
