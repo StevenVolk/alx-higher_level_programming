89c7a46724fc88a0293f037d376aeb3ee9e96279
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-12 09:05:19 -0700

    changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /* prints a message depending of the number of arguments passed */
-let len = process.argv.length;
+const len = process.argv.length;
 if (len === 2) {
 	console.log('No argument');
 } else if (len === 3) {
