164d949b3a7b8d4babb4675f9f3ff568d2dac5f4
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-12 13:36:08 -0700

    new file

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* prints x times “C is fun */
+const times = process.argv[2];
+
+for (let i = 0; i < times; ++i) {
+	console.log("C is fun");
+}
+if (!times) {
+	console.log("Missing number of occurrences");
+}
