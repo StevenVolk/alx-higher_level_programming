b65f9d1397d3a6b42f7ac1d9a705859692c40f07
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-12 11:19:19 -0700

    new

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let args = process.argv;
+let len = 0;
+args.forEach((val, index) => {
+	len++;
+	if (index > 1) {
+		console.log(val);
+	}
+});
+if (len === 2) {
+	console.log('No argument');
+}
