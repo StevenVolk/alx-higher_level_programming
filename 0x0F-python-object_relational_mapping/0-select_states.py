c425f1e98e57b971bbf9d04ce3053320b1e4d928
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 16:48:08 -0700

    new

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+a script that lists all states from the database hbtn_0e_0_usa
+
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+
+    accessing database
+
+    """
+
+    database = MYSQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+            database=argv[3], port=3306)
+    database.execute("SELECT * FROM states")
+    table = database.fetchall()
+    for row in table:
+        print(row)
