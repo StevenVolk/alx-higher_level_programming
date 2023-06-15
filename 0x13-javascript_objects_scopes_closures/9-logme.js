5c798382583e04dbb74065ccc2407d6bc1e115d7
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 12:32:03 -0700

    defined arguements

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* prints the number of arguments already printed and the new argument value */
 
-arguements = 0;
+let arguements = 0;
 exports.logMe = function (item) {
   console.log(arguements + ': ' + item);
   arguements++;
