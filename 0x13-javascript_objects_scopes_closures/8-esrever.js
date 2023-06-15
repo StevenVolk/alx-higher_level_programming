a7e47336294568d585e70f1e60229d31a23e8974
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 12:22:24 -0700

    new file

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* a function that returns the reversed version of a list */
+
+exports.esrever = function (list) {
+  let temp = "";
+  let len = list.length;
+  for (let i = 0; i < list.length / 2; i++) {
+    temp = list[i];
+    list[i] = list[len-1];
+    list[len-1] = temp;
+    len--;
+  }
+  return (list);
+}
