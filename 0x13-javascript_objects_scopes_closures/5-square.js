a51f888442e4f4df35c0be557e5369e0dbe8f6e2
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 11:11:17 -0700

    new file

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// class notation for defining your class and extends
+// must take 1 argument: size
+// The constructor of Rectangle must be called (by using super())
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super (size, size);
+  }
+};
