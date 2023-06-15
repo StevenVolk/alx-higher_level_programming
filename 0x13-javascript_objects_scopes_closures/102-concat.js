7a33d17597526d1ad0d19a9af33f20cf447db9c6
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 14:28:04 -0700

    new file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* a script that concats 2 files */
+
+var fs = require('fs');
+let data = '';
+
+data = data.concat(fs.readFileSync(process.argv[2]));
+data = data.concat(fs.readFileSync(process.argv[3]));
+
+fs.writeFileSync(process.argv[4], data);
