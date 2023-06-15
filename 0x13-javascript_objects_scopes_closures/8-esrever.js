782ede63ab40dba1b846761d8b9e576863bfb52a
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-15 12:24:21 -0700

    corrected spaces and single quotes

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,12 +2,12 @@
 /* a function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
-  let temp = "";
+  let temp = '';
   let len = list.length;
   for (let i = 0; i < list.length / 2; i++) {
     temp = list[i];
-    list[i] = list[len-1];
-    list[len-1] = temp;
+    list[i] = list[len - 1];
+    list[len - 1] = temp;
     len--;
   }
   return (list);
