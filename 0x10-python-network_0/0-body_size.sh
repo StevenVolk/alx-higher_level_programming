21c66b5ce4ccfb20cde04922c54fc1d9ee32b77e
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-30 16:11:57 -0700

    changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
+# takes a URL, sends a request to that URL and displays the body size
 curl -Is "$1" | grep "Content-Length:" | cut -d' ' -f2
