79ad29de5333de84edebeccf2061f7fadbf6daa4
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-30 17:49:30 -0700

    changes

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me and responds 'You got me!'
+# makes a request to 0.0.0.0:5000/catch_me and responds You got me!
 curl -sL 0.0.0.0:5000/catch_me
