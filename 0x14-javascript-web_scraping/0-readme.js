2a124831f292c5382702ba9ea53563d5b4339e47
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 16:50:15 -0700

    new

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function(error, data) {
-	if (error) {
-		console.log(error);
-	} else {
-		process.stdout.write(data);
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    process.stdout.write(data);
+  }
 });
