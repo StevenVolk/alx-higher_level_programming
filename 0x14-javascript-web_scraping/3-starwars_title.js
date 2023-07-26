362332addb3908f456f573cb8c4cd42509a6e2d5
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 17:25:09 -0700

    new file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
