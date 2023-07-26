7b31501c65305cdf71787d3fcaf80a0938abda18
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 19:29:44 -0700

    new

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, function (error, response, body) {
+  const chars = JSON.parse(body).characters;
+
+  for (let i = 0; i < chars.length; i++) {
+    request(chars[i], function (_err, _res, _body) {
+      console.log(JSON.parse(_body).name);
+    });
+  }
+});
