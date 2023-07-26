80cd9b0b1817643aa0650c13e8019041115405ca
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 22:07:09 -0700

    new

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,20 @@
 
 const request = require('request');
 const url = process.argv[2];
-let num_of_movies = 0;
+let numOfMovies = 0;
 let data;
 
 request(url, function (_err, _res, body) {
   data = JSON.parse(body).results;
   for (let i = 0; i < data.length; i++) {
     const chars = data[i].characters;
-    
     for (let j = 0; j < chars.length; j++) {
       const ch = chars[j];
       const charId = ch.split('/')[5];
       if (charId === '18') {
-        num_of_movies += 1;
+        numOfMovies += 1;
       }
     }
   }
-  console.log(num_of_movies);
+  console.log(numOfMovies);
 });
