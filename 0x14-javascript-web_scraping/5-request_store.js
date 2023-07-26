c793d03756ceeb6916c8ca2c7a3578fcbd2cae89
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 18:49:39 -0700

    new js file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,10 +3,10 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], function (error, response, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (error) {
-    if (error) {
-      console.log(error);
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (_err) {
+    if (_err) {
+      console.log(_err);
     }
   });
 });
