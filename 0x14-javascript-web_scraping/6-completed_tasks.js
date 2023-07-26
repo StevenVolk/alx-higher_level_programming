5222732f8b075c11392a6b5dad71c09c97741b5d
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 19:20:16 -0700

    new file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+let data;
+const dict = {};
+
+request(process.argv[2], function (_err, _res, body) {
+  if (_err) {
+    console.log(_err);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const id = result.userId;
+        if (!(id in dict)) {
+          dict[id] = 0;
+	}
+        dict[id] += 1;
+      }
+    });
+    console.log(dict);
+  }
+});
