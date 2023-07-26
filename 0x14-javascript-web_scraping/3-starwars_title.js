7cba1efc35d11902106b3fa962933947036ed0f6
Adedimeji Ojerinde <36421837+StevenVolk@users.noreply.github.com>
2023-07-25 20:30:08 -0400

    Delete 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-request(url, function (error, response, body) {
-  body = JSON.parse(body);
-  console.log(body.title);
-});
