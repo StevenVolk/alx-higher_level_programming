ad3b9ca88e9c20c979e4a9d02115438cc3ceb698
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 17:42:56 -0700

    new file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const id = process.argv[2]
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request(url, function (error, response, body) {
   const resJ = JSON.parse(body);
