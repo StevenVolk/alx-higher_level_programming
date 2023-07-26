b9b7a1db66cc0e20076a94929049c21d3c83cb36
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-25 17:13:33 -0700

    new

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-request(process.argv[2], function (error, response, body) {
-  console.log('code: ', response.statusCode);
+request(process.argv[2], function (error, response) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('code: ' + response.statusCode);
+	}
 });
