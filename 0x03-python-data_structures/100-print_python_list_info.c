a6ad9f4594bb41ef21e75dfac3e0303b90adf28d
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-03 15:28:17 -0800

    changes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,5 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
 #include <stdio.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - prints some basic info about Python lists
