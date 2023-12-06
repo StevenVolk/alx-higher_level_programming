0c62505537b4f2a9a8e7ae70ef71079663bdf9cf
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-06 06:03:04 -0800

    new_file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    rom_con = {"I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000}
+    num = []
+    result = 0
+    if roman_string == None or not(isinstance(roman_string, str)):
+        return 0
+    for r in range(0, len(roman_string)):
+        num.append(rom_con[roman_string[r]])
+        if len(num) > 1 and num[r-1] < num[r]:
+            result = result - (2 * num[r-1]) + num[r]
+        else:
+            result += num[r]
+    return result
