17b99434481f36a700097a8127837b06174cdf0c
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-06 06:21:31 -0800

    added comments

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
+    """
+    converts a Roman numeral to an integer
+    """
     rom_con = {"I": 1, "V": 5, "X": 10, "L": 50,
             "C": 100, "D": 500, "M": 1000}
     num = []
