21c8a3d2b96ed983020b70498ae6ff666759b0e9
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-06 20:27:13 -0800

    changed indents

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,7 @@ def roman_to_int(roman_string):
     """
     converts a Roman numeral to an integer
     """
-    rom_con = {"I": 1, "V": 5, "X": 10, "L": 50,
-              "C": 100, "D": 500, "M": 1000}
+    rom_con = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     num = []
     result = 0
     if roman_string is None or type(roman_string) is not str:
