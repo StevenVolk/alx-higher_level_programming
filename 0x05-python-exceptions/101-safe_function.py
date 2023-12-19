b672220064f5eb23e29d14d93a0f6f5b2b361ebe
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-12-18 23:23:01 -0800

    added spaces

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return(fct(*args))
+        return (fct(*args))
     except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
-        return(None)
+        return (None)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,4 +11,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             continue
     print()
-    return(j)
+    return (j)
